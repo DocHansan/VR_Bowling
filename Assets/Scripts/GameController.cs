04fe05fbbce489e6ed0eddceeb0631c320abc569
Ilya Razumov <dangerous-hanter@mail.ru>
2021-05-06 22:20:35 +1000

    Change variables to public in GameController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,13 +10,13 @@ public class GameController : MonoBehaviour
     public GameObject[] Balls;
     public GameObject Recipient;
     public int[] Throws = new int[21];
+    public int Throw = -1;
+    public int PlayerTry = 0;
+    public int DroppedPinsCount;
 
-    private int _PlayerTry = 0;
     private bool _IsGameBegin = false;
     private float _timer = 0;
     private Vector3 _PinRotation;
-    private int _DroppedPinsCount;
-    private int _Throw = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -42,44 +42,41 @@ public class GameController : MonoBehaviour
                 RespawnBalls();
                 _IsGameBegin = false;
                 ÑheckDroppedPins();
-                Throws[_Throw] = _DroppedPinsCount;
+                Throws[Throw] = DroppedPinsCount;
                 Recipient.GetComponent<ScoreDrawer>().UpdateThrows();
                 Recipient.GetComponent<ScoreDrawer>().UpdateFrames();
 
-                if (_PlayerTry == 2 || _DroppedPinsCount == 10)
+                if (PlayerTry == 2 || DroppedPinsCount == 10)
                 {
-                    if (_PlayerTry == 1 && _Throw < 18)
+                    if (PlayerTry == 1 && Throw < 18)
                     {
-                        _Throw += 1;
+                        Throw += 1;
                     }
                     RespawnPins();
-                    _PlayerTry = 0;
+                    PlayerTry = 0;
                 }
             }
         }
-        //Debug.Log(_IsGameBegin);
-        //Debug.Log(_PlayerTry);
-        //Debug.Log(_timer);
     }
 
     //Calls when ball in trigger zone
     public void BeginGame()
     {
-        if (!_IsGameBegin && _Throw < 19)
+        if (!_IsGameBegin && Throw < 19)
         {
-            _PlayerTry += 1;
+            PlayerTry += 1;
             _IsGameBegin = true;
-            _Throw += 1;
+            Throw += 1;
         }
-        else if (_Throw == 19 && (Throws[18] == 10 || Throws[18] + Throws[19] == 10))
+        else if (Throw == 19 && (Throws[18] == 10 || Throws[18] + Throws[19] == 10))
         {
-            _PlayerTry += 1;
+            PlayerTry += 1;
             _IsGameBegin = true;
-            _Throw += 1;
+            Throw += 1;
         }
     }
-    
-    void RespawnPins()
+
+    public void RespawnPins()
     {
         foreach (GameObject Pins in GameObject.FindGameObjectsWithTag("Pin"))
         {
@@ -88,7 +85,7 @@ public class GameController : MonoBehaviour
         Instantiate(Pins, PinsPosition.transform.position, Quaternion.identity);
     }
 
-    void RespawnBalls()
+    public void RespawnBalls()
     {
         foreach (GameObject Ball in GameObject.FindGameObjectsWithTag("Ball"))
         {
@@ -102,7 +99,7 @@ public class GameController : MonoBehaviour
 
     void ÑheckDroppedPins()
     {
-        _DroppedPinsCount = 0;
+        DroppedPinsCount = 0;
         foreach (GameObject Pins in GameObject.FindGameObjectsWithTag("Pin"))
         {
             foreach (Transform PinRotation in Pins.GetComponentsInChildren<Transform>())
@@ -112,7 +109,7 @@ public class GameController : MonoBehaviour
                     _PinRotation = PinRotation.rotation.eulerAngles;
                     if (_PinRotation[0] <= 250 || _PinRotation[0] >= 290)
                     {
-                        _DroppedPinsCount += 1;
+                        DroppedPinsCount += 1;
                         Destroy(PinRotation.gameObject);
                     }
                 }
