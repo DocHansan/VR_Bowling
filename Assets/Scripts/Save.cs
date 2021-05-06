3cc8cab384d05b731024c37e634785f4d674735a
Ilya Razumov <dangerous-hanter@mail.ru>
2021-05-06 22:19:15 +1000

    Add Save Script

---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Save
+{
+    public int[] Throws = new int[21];
+    public int Throw;
+    public int PlayerTry;
+    public int DroppedPinsCount;
+}
