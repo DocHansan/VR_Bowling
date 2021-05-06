dc4f7beac25243ad4487074cf9309728ec90bceb
Ilya Razumov <dangerous-hanter@mail.ru>
2021-05-06 22:21:14 +1000

    Add Save, Load and Qiut Game in MainMenuController

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,24 +1,80 @@
 using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject SaveObject;
 
     public void LoadLevel(int level)
     {
         SceneManager.LoadScene(level);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SaveGame()
+    {
+        Save SaveClass = CreateSaveClass();
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/Save.save");
+        bf.Serialize(file, SaveClass);
+        file.Close();
+    }
+
+    public void LoadGame()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+
+        FileStream file = File.Open(Application.persistentDataPath + "/Save.save", FileMode.Open);
+
+        Save SaveClass = (Save)bf.Deserialize(file);
+        file.Close();
+
+        SaveObject.GetComponent<GameController>().Throws = SaveClass.Throws;
+        SaveObject.GetComponent<GameController>().Throw = SaveClass.Throw;
+        SaveObject.GetComponent<GameController>().PlayerTry = SaveClass.PlayerTry;
+        SaveObject.GetComponent<GameController>().DroppedPinsCount = SaveClass.DroppedPinsCount;
+
+        SaveObject.GetComponent<GameController>().Recipient.GetComponent<ScoreDrawer>().UpdateThrows();
+        SaveObject.GetComponent<GameController>().Recipient.GetComponent<ScoreDrawer>().UpdateFrames();
+        SaveObject.GetComponent<GameController>().RespawnPins();
+        SaveObject.GetComponent<GameController>().RespawnBalls();
+
+        if (SaveClass.Throw % 2 == 0)
+        {
+            foreach (Transform PinOnScene in GameObject.FindGameObjectsWithTag("Pin")[1].GetComponentsInChildren<Transform>())
+            {
+                Debug.Log("Try");
+                Debug.Log(SaveClass.DroppedPinsCount);
+                if (PinOnScene != GameObject.FindGameObjectsWithTag("Pin")[1].GetComponentsInChildren<Transform>()[0])
+                {
+                    if (SaveClass.DroppedPinsCount != 0)
+                    {
+                        SaveClass.DroppedPinsCount -= 1;
+                        Destroy(PinOnScene.gameObject);
+                    }
+                }
+            }
+        }
+    }
+
+    private Save CreateSaveClass()
+    {
+        Save SaveClass = new Save();
+
+        SaveClass.Throws = SaveObject.GetComponent<GameController>().Throws;
+        SaveClass.Throw = SaveObject.GetComponent<GameController>().Throw;
+        SaveClass.PlayerTry = SaveObject.GetComponent<GameController>().PlayerTry;
+        SaveClass.DroppedPinsCount = SaveObject.GetComponent<GameController>().DroppedPinsCount;
+        
+        return SaveClass;
     }
 }
